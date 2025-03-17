078ce67aaf28cdceac319905a0e156c548e631bf
dariaar <droso00@fesb.hr>
2025-03-17 15:35:17 +0100

    vercel

--- frontend/src/PersonalPage.js
+++ frontend/src/PersonalPage.js
@@ -24,7 +24,7 @@ function PersonalPage() {
     setProgress({ started: true, pc: 0 });
 
     try {
-      const res = await axios.post("https://hcr.onrender.com/upload", fd, {
+      const res = await axios.post("https://hcr-1-eight.vercel.app/upload", fd, {
         headers: { "Content-Type": "multipart/form-data" },
         onUploadProgress: (progressEvent) => {
           setProgress({ started: true, pc: Math.round((progressEvent.loaded * 100) / progressEvent.total) });
