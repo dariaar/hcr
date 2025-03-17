1567f6781fe3875e020c33aefb669ed71e072864
dariaar <droso00@fesb.hr>
2025-03-17 14:14:45 +0100

    poetry

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Omogućavamo CORS za React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://hcr-1.vercel.app", "http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
