7bc4e226a25c198f9d2123f7e0f34d2bb81dab1a
dariaar <droso00@fesb.hr>
2025-05-06 15:04:52 +0200

    fastapi middleware

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://hcr-1.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
